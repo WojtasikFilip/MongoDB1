0c5bd9f3cbc0c0fc9ea4c390c740a34b404f2ebf
Filip <wojtasik.f03@htlwienwest.at>
2021-06-16 22:52:53 +0200

    Alle Dogs aus JSON File inserted

--- loadData.js
+++ loadData.js
@@ -1,16 +1,30 @@
+const fs = require('fs');
 require('./db/connect');
 require('colors');
 const { Dog } = require('./model/schemas.js');
 
+const allDogs = JSON.parse(fs.readFileSync('./dogs.json', 'utf8'));
+
 (async () => {
   try {
-    const p1 = Dog.create({ name: 'Bello', born: '2020-12-01' });
-    const p2 = Dog.create({ name: 'Beisser', born: '2020-12-02' });
-    await Promise.all([p1, p2]);
+    await Dog.insertMany(allDogs);
     console.log('Data loaded');
   } catch (error) {
-    console.error(`Error ====> ${error.message.red}`);
+    console.error(`Error => ${error.message.red}`);
   } finally {
     process.exit('SIGINT');
   }
 })();
+
+// (async () => {
+//   try {
+//     const p1 = Dog.create({ name: 'Bello', born: '2020-12-01' });
+//     const p2 = Dog.create({ name: 'Beisser', born: '2020-12-02' });
+//     await Promise.all([p1, p2]);
+//     console.log('Data loaded');
+//   } catch (error) {
+//     console.error(`Error ====> ${error.message.red}`);
+//   } finally {
+//     process.exit('SIGINT');
+//   }
+// })();
